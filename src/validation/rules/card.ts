64e34ebef0bd0bdd5036c54734abbaccdbab1dd9
Gabriel Mateos <gmdcwork@gmail.com>
2020-01-10 10:26:02 +0100

    fix: card number regex validation

--- src/validation/rules/card.ts
+++ src/validation/rules/card.ts
@@ -1,4 +1,4 @@
 import * as common from './common';
 
-export const NumberRegex = /^[1-9]|[1-4]\d|5[0-4]$/; // 54 cards max
+export const NumberRegex = /^([1-9]|[1-4]\d|5[0-4])$/; // 54 cards max
 export const TypeRegex = common.NameRegex;
